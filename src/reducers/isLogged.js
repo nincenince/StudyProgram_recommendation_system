b1aa98a6be19904cf77c7229e64589d68af39f51
pong10 <pong.1999@hotmail.com>
2021-02-18 04:16:04 +0700

    Create login reducer

---
+++ src/reducers/isLogged.js
@@ -0,0 +1,13 @@
+const loggedReducer = (state = false, action) => {
+    switch(action.type){
+        case 'SIGN_IN':
+            state = true;
+            return state;
+        case 'SIGN_OUT':
+            state = false
+            return state
+        default: 
+            return state;
+    }
+};
+export default loggedReducer;
