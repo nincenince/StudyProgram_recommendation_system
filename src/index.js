bff931ad602920d3b18ecfaddede69b221593477
pong10 <pong.1999@hotmail.com>
2021-02-18 04:16:57 +0700

    Add store to app

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 ReactDOM.render(
-
-    <App /> ,document.getElementById('root')
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
 );
-
