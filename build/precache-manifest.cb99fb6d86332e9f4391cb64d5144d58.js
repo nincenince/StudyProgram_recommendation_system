a1aeb8ccd11eddb64aa9fb1a1ffeaffe8be12093
pong10 <pong.1999@hotmail.com>
2021-02-18 02:20:49 +0700

    1st deploy

---
+++ build/precache-manifest.cb99fb6d86332e9f4391cb64d5144d58.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2c2e7b34229b495db3f8dde183a858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58eb1842f60ee20d4ca5",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "11341d22525f95f142aa",
+    "url": "/static/css/main.688b6121.chunk.css"
+  },
+  {
+    "revision": "58eb1842f60ee20d4ca5",
+    "url": "/static/js/2.7e1f3bb6.chunk.js"
+  },
+  {
+    "revision": "9d7baa484de148efea1c408ede69d272",
+    "url": "/static/js/2.7e1f3bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11341d22525f95f142aa",
+    "url": "/static/js/main.91f069fe.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  },
+  {
+    "revision": "8a58b619d845d34216c6f1f004d4babd",
+    "url": "/static/media/CIE.8a58b619.jpg"
+  },
+  {
+    "revision": "3ece334e9dffb731b0b3eaad13cbfc9e",
+    "url": "/static/media/IMG_0947.3ece334e.JPG"
+  },
+  {
+    "revision": "5e77403a42f44d410382fa85c4f53535",
+    "url": "/static/media/bg2.5e77403a.jpg"
+  },
+  {
+    "revision": "b589aca5d844b3f1cc7fbd01aa18c1b1",
+    "url": "/static/media/ce.b589aca5.jpg"
+  },
+  {
+    "revision": "6a2e0af74cd77c20139d11455800e27a",
+    "url": "/static/media/elec.6a2e0af7.png"
+  },
+  {
+    "revision": "b26e0a59dc0b4ebcc867933466b98586",
+    "url": "/static/media/ie.b26e0a59.png"
+  },
+  {
+    "revision": "ae7b181d2cb25b99c6bdcd144a7362c2",
+    "url": "/static/media/perstest.ae7b181d.png"
+  },
+  {
+    "revision": "11a4c2dbe19c814fb90923cdea86d02a",
+    "url": "/static/media/results.11a4c2db.png"
+  },
+  {
+    "revision": "3e180854b34478722aba62c5c019043c",
+    "url": "/static/media/robo.3e180854.png"
+  },
+  {
+    "revision": "8789c4442a27b215b3bb82faf11bde4c",
+    "url": "/static/media/se.8789c444.png"
+  },
+  {
+    "revision": "54378673e697e802d850fc6598f5f7f7",
+    "url": "/static/media/signin.54378673.png"
+  },
+  {
+    "revision": "40e9d6d2c8931503167d4d6eb7387e01",
+    "url": "/static/media/signup.40e9d6d2.png"
+  }
+]);
