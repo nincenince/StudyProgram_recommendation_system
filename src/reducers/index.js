7bffaed0d2e39ddb3d6a4d9c95f5a77941e0b7a4
pong10 <pong.1999@hotmail.com>
2021-02-25 01:00:35 +0700

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
 import tokenReducer from './token';
 import loggedReducer from './isLogged';
 import personal_info_Reducer from './personal_info';
+import education_info_Reducer from './education_info';
+import personality_info_Reducer from './personality_info';
 import { combineReducers } from 'redux';
 
 const allReducer = combineReducers({
     token : tokenReducer,
     isLogged : loggedReducer,
     personal : personal_info_Reducer,
+    education : education_info_Reducer,
+    personality: personality_info_Reducer,
 });
 export default allReducer;
