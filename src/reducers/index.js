325ad0ed3699199928d784f5e442cabec37da379
pong10 <pong.1999@hotmail.com>
2021-02-28 20:38:20 +0700

    Add come-from

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import loggedReducer from './isLogged';
 import personal_info_Reducer from './personal_info';
 import education_info_Reducer from './education_info';
 import personality_info_Reducer from './personality_info';
+import comefromReducer from './comefrom';
 import { combineReducers } from 'redux';
 
 const allReducer = combineReducers({
@@ -11,5 +12,6 @@ const allReducer = combineReducers({
     personal : personal_info_Reducer,
     education : education_info_Reducer,
     personality: personality_info_Reducer,
+    comefrom: comefromReducer,
 });
 export default allReducer;
