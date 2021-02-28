dfbdd37c954a54359048ba59b4a9230faed44f91
pong10 <pong.1999@hotmail.com>
2021-02-28 20:37:29 +0700

    Add Signup functionality

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import axios from 'axios';
 import './SignUp.css'
 import bg from '../images/IMG_0947.JPG'; 
 import { Col,Row, Container , Image, Jumbotron, Button, Form} from "react-bootstrap";
+
 
   
 class SignUp extends React.Component {
@@ -9,15 +11,15 @@ class SignUp extends React.Component {
     super();
     this.state = {
       input: {},
-      errors: {}
+      errors: {},
+      response: {},
     };
-    
+     
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
      
   handleChange(event) {
-    // event.preventDefault();
     let input = this.state.input;
     input[event.target.name] = event.target.value;
   
@@ -29,36 +31,38 @@ class SignUp extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
   
-    if(this.validate()){
-        console.log(this.state);
-  
+    if(this.validate()){ 
         let input = {};
-        input["username"] = "";
-        input["last_name"]= "";
-        input["email"] = "";
-        input["password"] = "";
-        input["confirm_password"] = "";
+        input["username"] = "test_user";
+        input["last_name"]= "test_user";
+        input["gender"]="Male";
+        input["age"]=32;
+        input["email"] = " ";
+        input["password"] = " ";
+        input["confirm_password"] = " ";
         this.setState({input:input});
-  
-        alert('Demo Form is submited');
+ 
     }
   }
   
-  validate(){
+  async validate(){
       let input = this.state.input;
       let errors = {};
       let isValid = true;
   
-      if (!input["username"]) {
+      if (!input["firstname"]) {
+        console.log('1');
         isValid = false;
-        errors["username"] = "Please enter your username.";
+        errors["username"] = "Please enter your firstname.";
       }
-      if (!input["last_name"]) {
+      if (!input["lastname"]) {
+        console.log('2');
         isValid = false;
-        errors["last_name"] = "Please enter your last name.";
+        errors["lastname"] = "Please enter your lastname.";
       }
   
       if (!input["email"]) {
+        console.log('3');
         isValid = false;
         errors["email"] = "Please enter your email Address.";
       }
@@ -67,17 +71,31 @@ class SignUp extends React.Component {
           
         var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
         if (!pattern.test(input["email"])) {
+          console.log('4');
           isValid = false;
           errors["email"] = "Please enter valid email address.";
         }
       }
-  
+      
+      if (!input["gender"]) {
+        console.log('5');
+        isValid = false;
+        errors["gender"] = "Please select your gender.";
+      }
+      if (input["age"] == null) {
+        console.log('6');
+        isValid = false;
+        errors["age"] = "Please enter your age.";
+      }
+
       if (!input["password"]) {
+        console.log('7');
         isValid = false;
         errors["password"] = "Please enter your password.";
       }
   
       if (!input["confirm_password"]) {
+        console.log('8');
         isValid = false;
         errors["confirm_password"] = "Please enter your confirm password.";
       }
@@ -85,6 +103,7 @@ class SignUp extends React.Component {
       if (typeof input["password"] !== "undefined" && typeof input["confirm_password"] !== "undefined") {
           
         if (input["password"] !== input["confirm_password"]) {
+          console.log('9');
           isValid = false;
           errors["password"] = "Passwords don't match.";
         }
@@ -93,8 +112,33 @@ class SignUp extends React.Component {
       this.setState({
         errors: errors
       });
-  
+
+      if (isValid) {
+        let payload = {
+          'email' : input['email'],
+          'password' : input['password'],
+          'age' : input['age'],
+          'sex' : input['gender'],
+          'firstname' : input['firstname'],
+          'lastname' : input['lastname']
+        }
+        console.log(payload);
+        //response = await axios.post("https://spr-system.herokuapp.com/signup/", payload);
+        this.response = await axios.post("http://127.0.0.1:8000/signup/", payload);
+      }
+      else {
+        return isValid;
+      }
+      if (this.response.data['status'] === true) {
+        console.log(this.response.data)
+        alert(this.response.data['message'])
+        this.props.history.push("/SignIn")
+      }
+      else {
+        alert(this.response.data['message'])
+      }
       return isValid;
+
   }
      
   render() {
@@ -103,63 +147,70 @@ class SignUp extends React.Component {
           <Container>
             <Col></Col>
             <Col>
-        <form onSubmit={this.handleSubmit}>
 
           <Form onSubmit={this.handleSubmit}>
             <h1 style={{ fontSize:'2vw'}}>Sign Up</h1>
-            <Form.Group >
-              <Form.Label style={{ fontSize:'1vw'}}>Username</Form.Label>
-              <Form.Control style={{ fontSize:'1vw'}} type="text" name="username"
-              placeholder="Enter Username" value={this.state.input.username} onChange={this.handleChange} />
+            <Form.Group controlId="formfirstname">
+              <Form.Label style={{ fontSize:'1vw'}}>Firstname</Form.Label>
+              <Form.Control style={{ fontSize:'1vw'}} type="text" name="firstname"
+              placeholder="Enter Firstname" value={this.state.input.firstname} onChange={this.handleChange} />
               <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
-              {this.state.errors.username}
+              {this.state.errors.firstname}
               </Form.Text>
             </Form.Group>
 
-            <Form.Group >
+            <Form.Group controlId="formlastname">
+              <Form.Label style={{ fontSize:'1vw'}}>Lastname</Form.Label>
+              <Form.Control style={{ fontSize:'1vw'}} type="text" name="lastname"
+              placeholder="Enter Lastname" value={this.state.input.lastname} onChange={this.handleChange} />
+              <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+              {this.state.errors.lastname}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formemail">
               <Form.Label style={{ fontSize:'1vw'}}>Email address</Form.Label>
               <Form.Control style={{ fontSize:'1vw'}} type="email" name="email"
-               placeholder="Enter email" value={this.state.input.email} onChange={this.handleChange} />
+              placeholder="Enter email" value={this.state.input.email} onChange={this.handleChange} />
               <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
               {this.state.errors.email}
               </Form.Text>
             </Form.Group>
 
 
-          <Form.Row>   <Form.Label style={{ fontSize:'1vw'}}>Date of Birth</Form.Label></Form.Row>
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Control as="select" defaultValue="DD" style={{ fontSize:'1vw'}}>
-               <option style={{ fontSize:'1vw'}}>DD</option>
-               <option style={{ fontSize:'1vw'}}>01</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Control as="select" defaultValue="MM" style={{ fontSize:'1vw'}}>
-               <option style={{ fontSize:'1vw'}}>MM</option>
-               <option style={{ fontSize:'1vw'}}>01</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Control as="select" defaultValue="YY" style={{ fontSize:'1vw'}}>
-               <option style={{ fontSize:'1vw'}}>YY</option>
-               <option style={{ fontSize:'1vw'}}>01</option>
-              </Form.Control>
+            <Form.Group controlId="formage">
+              <Form.Label style={{ fontSize:'1vw'}}>Age</Form.Label>
+              <Form.Control style={{ fontSize:'1vw'}} type="number" name="age" value={this.state.input.age} onChange={this.handleChange} />
+              <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+              {this.state.errors.age}
+              </Form.Text>
             </Form.Group>
 
-            </Form.Row> 
+            <Form.Group controlId="formgender">
+              <Form.Label style={{ fontSize:'1vw'}}>Gender</Form.Label>
+              <Form.Control style={{ fontSize:'1vw'}} as="select" name="gender" value={this.state.input.gender} onChange={this.handleChange} >
+                <option style={{ fontSize:'1vw'}}>{null}</option>
+                    <option style={{ fontSize:'1vw'}}>Female</option>
+                    <option style={{ fontSize:'1vw'}}>Male</option>
+                    <option style={{ fontSize:'1vw'}}>Other</option>
+              </Form.Control>
+              <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+              {this.state.errors.gender}
+              </Form.Text>
+            </Form.Group>
 
-            <Form.Group >
+            <br></br>
+            <Form.Group controlId="formpassword">
               <Form.Label style={{ fontSize:'1vw'}}>Password</Form.Label>
               <Form.Control style={{ fontSize:'1vw'}} type="password" name="password"
-               placeholder="Enter password" 
+              placeholder="Enter password" 
               value={this.state.input.password} onChange={this.handleChange} />
               <Form.Text style={{color: "red", fontSize: "0.75vw"}} className="text-danger">
               {this.state.errors.password}
               </Form.Text>
             </Form.Group>
-
-            <Form.Group >
+            <br></br>
+            <Form.Group controlId="formconfirmpassword">
               <Form.Label style={{ fontSize:'1vw'}}>Confirm Password</Form.Label>
               <Form.Control style={{ fontSize:'1vw'}} type="password" name="confirm_password"
               placeholder="Enter password" 
@@ -168,16 +219,15 @@ class SignUp extends React.Component {
               {this.state.errors.confirm_password}
               </Form.Text>
             </Form.Group>
-     
+            <br></br>
 
             <Button 
             style={{display:"flex", alignItems:"center", justifyContent:"center", fontSize:"1vw"}}
             variant="primary" type="submit" value="Submit">
               Sign Up
             </Button>
-           
-           </Form>
-           </form>
+          
+          </Form>
           </Col>
           <Col></Col>
         </Container>
