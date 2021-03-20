ae45d2fc9986a8921a60e3df7c3f45f89f23048d
pong10 <pong.1999@hotmail.com>
2021-03-20 19:41:02 +0700

    Fix gat/pat issue

--- src/pages/EduInformation.js
+++ src/pages/EduInformation.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import {Link } from "react-router-dom";
+import React from 'react';
+// import {Link } from "react-router-dom";
 import './AccInfo.css';
-import { Col,Row, Container , Image, Form, Button} from "react-bootstrap";
+import { Row, Container ,Form, Button} from "react-bootstrap";
 
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch} from 'react-redux';
 import { update_edu } from '../actions';
-import education_info_Reducer from '../reducers/education_info';
+// import education_info_Reducer from '../reducers/education_info';
 
 
 function EduInformation (props) {
@@ -46,14 +46,14 @@ function EduInformation (props) {
   const [newielts, setnewielts] = useState(education.EduInfo[11].ScoreList[0].Score);
 
   const [gpaxerrors, setgpaxerrors] = useState('');
-  const [gaterrors, setgaterrors] = useState('');
-  const [pat1errors, setpat1errors] = useState('');
-  const [pat2errors, setpat2errors] = useState('');
-  const [pat3errors, setpat3errors] = useState('');
-  const [pat4errors, setpat4errors] = useState('');
-  const [pat5errors, setpat5errors] = useState('');
-  const [pat6errors, setpat6errors] = useState('');
-  const [pat7errors, setpat7errors] = useState('');
+  // const [gaterrors, setgaterrors] = useState('');
+  // const [pat1errors, setpat1errors] = useState('');
+  // const [pat2errors, setpat2errors] = useState('');
+  // const [pat3errors, setpat3errors] = useState('');
+  // const [pat4errors, setpat4errors] = useState('');
+  // const [pat5errors, setpat5errors] = useState('');
+  // const [pat6errors, setpat6errors] = useState('');
+  // const [pat7errors, setpat7errors] = useState('');
   const [satmatherrors, setsatmatherrors] = useState('');
   const [satengerrors, setsatengerrors] = useState('');
   const [ieltserrors, setieltserrors] = useState('');
@@ -61,7 +61,7 @@ function EduInformation (props) {
   const [codes, setcodes] = useState(0);
 
   const redirectback = () => {
-    if(comefrom == 'dashboard'){
+    if(comefrom === 'dashboard'){
       props.history.push("/Dashboard");
     }
     else{
@@ -70,65 +70,72 @@ function EduInformation (props) {
   }
   const handlefSubmit = (event) => {
     event.preventDefault();
-    if(newgpax == 0){
+    if(newgpax === 0){
       setnewgpax(education.EduInfo[0].ScoreList[0].Score)
     }
-    if(newsatmath == 0){
+    if(newsatmath === 0){
       setnewsatmath(education.EduInfo[9].ScoreList[0].Score)
     }
-    if(newsateng == 0){
+    if(newsateng === 0){
       setnewsateng(education.EduInfo[10].ScoreList[0].Score)
     }
-    if(newielts == 0){
+    if(newielts === 0){
       setnewielts(education.EduInfo[11].ScoreList[0].Score)
     }
     validate(token,true,newgpax,false,null,false,null,false,null,false,null,false,null,false,null,false,null,false,null,true,newsatmath,true,newsateng,true,newielts);
   }
   const handlegpSubmit = (event) => {
     event.preventDefault();
-    if(newgat == 0){
+    if(newgat === 0){
       setnewgat(education.EduInfo[1].ScoreList[0].Score)
     }
-    if(newpat1 == 0){
+    if(newpat1 === 0){
       setnewpat1(education.EduInfo[2].ScoreList[0].Score)
     }
-    if(newpat2 == 0){
+    if(newpat2 === 0){
       setnewpat2(education.EduInfo[3].ScoreList[0].Score)
     }
-    if(newpat3 == 0){
+    if(newpat3 === 0){
       setnewpat3(education.EduInfo[4].ScoreList[0].Score)
     }
-    if(newpat4 == 0){
+    if(newpat4 === 0){
       setnewpat4(education.EduInfo[5].ScoreList[0].Score)
     }
-    if(newpat5 == 0){
+    if(newpat5 === 0){
       setnewpat5(education.EduInfo[6].ScoreList[0].Score)
     }
-    if(newpat6 == 0){
+    if(newpat6 === 0){
       setnewpat6(education.EduInfo[7].ScoreList[0].Score)
     }
-    if(newpat7 == 0){
+    if(newpat7 === 0){
       setnewpat7(education.EduInfo[8].ScoreList[0].Score)
     }
-    
+    console.log(newgat);
+    console.log(newpat1);
+    console.log(newpat2);
+    console.log(newpat3);
+    console.log(newpat4);
+    console.log(newpat5);
+    console.log(newpat6);
+    console.log(newpat7);
     validate(token,false,null,true,newgat,true,newpat1,true,newpat2,true,newpat3,true,newpat4,true,newpat5,true,newpat6,true,newpat7,false,null,false,null,false,null);
   }
   
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
     
-    if(this.validate()){
-        console.log(this.state);
+  //   if(this.validate()){
+  //       console.log(this.state);
   
-        let input = {};
-        input["email"] = "";
-        input["password"] = "";
-        input["confirm_password"] = "";
-        this.setState({input:input});
+  //       let input = {};
+  //       input["email"] = "";
+  //       input["password"] = "";
+  //       input["confirm_password"] = "";
+  //       this.setState({input:input});
   
-        alert('Demo Form is submited');
-    }
-  }
+  //       alert('Demo Form is submited');
+  //   }
+  // }
   
   const validate = async (tk,gpc,ngp,gc,ng,p1c,np1,p2c,np2,p3c,np3,p4c,np4,p5c,np5,p6c,np6,p7c,np7,smc,nsm,sec,nse,iec,nie) => {
 
@@ -171,48 +178,48 @@ function EduInformation (props) {
       nsm = parseInt(nsm);
       nse = parseInt(nse);
     }
-    if(codes === 2) {
-      if (!input["gat"] ) {
-        isValid = false;
-        setgaterrors("Please enter your gat.");
-      }
-      if (!input["pat1"] ) {
-        isValid = false;
-        setpat1errors("Please enter your pat1.");
-      }
-      if (!input["pat2"] ) {
-        isValid = false;
-        setpat2errors("Please enter your pat2.");
-      }
-      if (!input["pat3"] ) {
-        isValid = false;
-        setpat3errors("Please enter your pat3.");
-      }
-      if (!input["pat4"] ) {
-        isValid = false;
-        setpat4errors("Please enter your pat4.");
-      }
-      if (!input["pat5"] ) {
-        isValid = false;
-        setpat5errors("Please enter your pat5.");
-      }
-      if (!input["pat6"] ) {
-        isValid = false;
-        setpat6errors("Please enter your pat6.");
-      }
-      if (!input["pat7"] ) {
-        isValid = false;
-        setpat7errors("Please enter your pat7.");
-      }
-      ng = parseInt(ng);
-      np1 = parseInt(np1);
-      np2 = parseInt(np2);
-      np3 = parseInt(np3);
-      np4 = parseInt(np4);
-      np5 = parseInt(np5);
-      np6 = parseInt(np6);
-      np7 = parseInt(np7);
-    }
+    // if(codes === 2) {
+    //   if (!input["gat"] ) {
+    //     isValid = false;
+    //     setgaterrors("Please enter your gat.");
+    //   }
+    //   if (!input["pat1"] ) {
+    //     isValid = false;
+    //     setpat1errors("Please enter your pat1.");
+    //   }
+    //   if (!input["pat2"] ) {
+    //     isValid = false;
+    //     setpat2errors("Please enter your pat2.");
+    //   }
+    //   if (!input["pat3"] ) {
+    //     isValid = false;
+    //     setpat3errors("Please enter your pat3.");
+    //   }
+    //   if (!input["pat4"] ) {
+    //     isValid = false;
+    //     setpat4errors("Please enter your pat4.");
+    //   }
+    //   if (!input["pat5"] ) {
+    //     isValid = false;
+    //     setpat5errors("Please enter your pat5.");
+    //   }
+    //   if (!input["pat6"] ) {
+    //     isValid = false;
+    //     setpat6errors("Please enter your pat6.");
+    //   }
+    //   if (!input["pat7"] ) {
+    //     isValid = false;
+    //     setpat7errors("Please enter your pat7.");
+    //   }
+    //   ng = parseInt(ng);
+    //   np1 = parseInt(np1);
+    //   np2 = parseInt(np2);
+    //   np3 = parseInt(np3);
+    //   np4 = parseInt(np4);
+    //   np5 = parseInt(np5);
+    //   np6 = parseInt(np6);
+    //   np7 = parseInt(np7);
+    // }
     // alert(isValid);
     if(isValid) {
       let payload = {
@@ -255,7 +262,7 @@ function EduInformation (props) {
       dispatch(update_edu(response.data['info']));
       alert(response.data['message']);
       response = {};
-      if(comefrom == 'dashboard'){
+      if(comefrom === 'dashboard'){
         props.history.push("/Dashboard");
       }
       else{
@@ -355,58 +362,58 @@ function EduInformation (props) {
                 <Form.Label style={{ fontSize:'1vw'}}>GAT - General Aptitude Test</Form.Label>
                   <Form.Group  controlId="forgat">
                     <Form.Control type="number" min="0" max="300" step="1" style={{ fontSize:'1vw'}} placeholder={newgat} value={newgat} onChange={e => setnewgat(e.target.value)}/>
-                    <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+                    {/* <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
                       {gaterrors}
-                    </Form.Text>
+                    </Form.Text> */}
                   </Form.Group>
                   <Form.Label style={{ fontSize:'1vw'}}>PAT 1 - Aptitude in Mathematics</Form.Label>
                   <Form.Group  controlId="forpat1">
                     <Form.Control type="number" min="0" max="300" step="1" style={{ fontSize:'1vw'}} placeholder={newpat1} value={newpat1} onChange={e => setnewpat1(e.target.value)}/>
-                    <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+                    {/* <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
                       {pat1errors}
-                    </Form.Text>
+                    </Form.Text> */}
                   </Form.Group>
                   <Form.Label style={{ fontSize:'1vw'}}>PAT 2 - Aptitude in Scientifics</Form.Label>
                   <Form.Group  controlId="forpat2">
                     <Form.Control type="number" min="0" max="300" step="1" style={{ fontSize:'1vw'}} placeholder={newpat2} value={newpat2} onChange={e => setnewpat2(e.target.value)}/>
-                    <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+                    {/* <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
                       {pat2errors}
-                    </Form.Text>
+                    </Form.Text> */}
                   </Form.Group>
                   <Form.Label style={{ fontSize:'1vw'}}>PAT 3 - Aptitude in Engineering</Form.Label>
                   <Form.Group  controlId="forpat3">
                     <Form.Control type="number" min="0" max="300" step="1" style={{ fontSize:'1vw'}} placeholder={newpat3} value={newpat3} onChange={e => setnewpat3(e.target.value)}/>
-                    <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+                    {/* <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
                       {pat3errors}
-                    </Form.Text>
+                    </Form.Text> */}
                   </Form.Group>
                   <Form.Label style={{ fontSize:'1vw'}}>PAT 4 - Aptitude in Architectural</Form.Label>
                   <Form.Group  controlId="forpat4">
                     <Form.Control type="number" min="0" max="300" step="1" style={{ fontSize:'1vw'}} placeholder={newpat4} value={newpat4} onChange={e => setnewpat4(e.target.value)}/>
-                    <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+                    {/* <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
                       {pat4errors}
-                    </Form.Text>
+                    </Form.Text> */}
                   </Form.Group>
                   <Form.Label style={{ fontSize:'1vw'}}>PAT 5 - Aptitude in Teacher professional</Form.Label>
                   <Form.Group  controlId="forpat5">
                     <Form.Control type="number" min="0" max="300" step="1" style={{ fontSize:'1vw'}} placeholder={newpat5} value={newpat5} onChange={e => setnewpat5(e.target.value)}/>
-                    <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+                    {/* <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
                       {pat5errors}
-                    </Form.Text>
+                    </Form.Text> */}
                   </Form.Group>
                   <Form.Label style={{ fontSize:'1vw'}}>PAT 6 - Aptitude in Arts</Form.Label>
                   <Form.Group  controlId="forpat6">
                     <Form.Control type="number" min="0" max="300" step="1" style={{ fontSize:'1vw'}} placeholder={newpat6} value={newpat6} onChange={e => setnewpat6(e.target.value)}/>
-                    <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+                    {/* <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
                       {pat6errors}
-                    </Form.Text>
+                    </Form.Text> */}
                   </Form.Group>
                   <Form.Label style={{ fontSize:'1vw'}}>PAT 7 - Aptitude in Foreign language proficiency (French, German, Chinese, Japanese, Arabic, Pali)</Form.Label>
                   <Form.Group  controlId="forpat7">
                     <Form.Control type="number" min="0" max="300" step="1" style={{ fontSize:'1vw'}} placeholder={newpat7} value={newpat7} onChange={e => setnewpat7(e.target.value)}/>
-                    <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
+                    {/* <Form.Text style={{color: "red", fontSize: "0.75vw"}} >
                       {pat7errors}
-                    </Form.Text>
+                    </Form.Text> */}
                   </Form.Group>
                 
 
@@ -419,7 +426,7 @@ function EduInformation (props) {
 
                 <hr></hr>
                 <br></br>
-                <Button className="btn btn-danger btn-lg btn-block" Style="fontSize:'1vw';" onClick={redirectback}>
+                <Button className="btn btn-danger btn-lg btn-block" Style={{ fontSize:'1vw'}} onClick={redirectback}>
                   Back
                 </Button>
                 <br></br>
