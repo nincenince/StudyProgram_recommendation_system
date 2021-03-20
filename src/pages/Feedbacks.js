ba0b4105e41f747d831b6df985d3f7d03d75a981
pong10 <pong.1999@hotmail.com>
2021-03-20 19:41:16 +0700

    Update to get info from server

--- src/pages/Feedbacks.js
+++ src/pages/Feedbacks.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import {Link } from "react-router-dom";
+import React from 'react';
+// import {Link } from "react-router-dom";
 import './Feedbacks.css'
-import { Col, Row, Container , Button, Card, Form, FormControl, Navbar} from "react-bootstrap";
-import Feedback from 'react-bootstrap/esm/Feedback';
-import Feedbackk from "../Feedbacks";
+import { Col} from "react-bootstrap";
+// import Feedback from 'react-bootstrap/esm/Feedback';
+// import Feedbackk from "../Feedbacks";
 import { Scrollbars } from 'rc-scrollbars';
+import {  useEffect, useState } from 'react';
+import axios from 'axios';
+function Feedbacks(props){
+    const [feedbackk, setfeedbackk] = useState([]);
+    const [found, setfound] = useState(true);
+    useEffect(() => {
+      async function get_feedback () {
+        const res = await axios.get('https://spr-system.herokuapp.com/get_feedback/');
+        //const res = await axios.get('http://127.0.0.1:8000/get_feedback/');
+        let response = res.data['Feedbacks'];
+        setfeedbackk(response);
+        setfound(true);
+      }
+      get_feedback();
+      // console.log(response);
+    },[found])
 
-class Feedbacks extends Component{
-    render(){
-  
-        return(
+    return(
       <>
 
 
@@ -17,27 +30,18 @@ class Feedbacks extends Component{
       style={{ fontSize:'2vw', margin:'2%', marginLeft:'10%'}}
         >Feedbacks from our users!</h2>
     
-  
-    
-
     <Col >
 
           <div style={{border:'solid', borderWidth:'thin', marginLeft:'10%',marginRight:'10%', marginTop:'4%'}}>
           <Scrollbars style={{ width: '100vw', height: '65vh'}}>
                 {
-                  Feedbackk.FeedBacks.map((Info) => {
+                  feedbackk.map((Info) => {
                     return (
-                      <div style={{border:'solid', borderWidth:'thin', borderRadius:'15px',margin:'5%'}} >
-                        <p 
-                        style={{ fontSize:'1vw', fontWeight:'900', paddingLeft:'5%', paddingTop:'2%'}} >
-                          {Info.Programs}</p>
-
-                          {
-                                  <p 
-                                  style={{ fontSize:'1vw', paddingLeft:'10%',paddingRight:'2%', paddingBottom:'3%'}} >
-                                    {Info.User_Feedback}
-                                  </p>
-                          }
+                      <div style={{border:'solid', borderWidth:'thin', borderRadius:'15px',margin:'5%'}} key={Info.User_Feedback}>
+                        <p style={{ fontSize:'1vw', fontWeight:'900', paddingLeft:'5%', paddingTop:'2%'}} >{Info.Title} - {Info.Rating}</p>
+                        {
+                          <p style={{ fontSize:'1vw', paddingLeft:'10%',paddingRight:'2%', paddingBottom:'3%'}} >{Info.User_Feedback}</p>
+                        }
                 
                       </div>
                      
@@ -58,7 +62,6 @@ class Feedbacks extends Component{
 
 
     )
-  }
 }
 
 export default Feedbacks;
