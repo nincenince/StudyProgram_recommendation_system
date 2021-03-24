4d63903aba0c1fbf7bcf1b89c5511f5fde88482c
pong10 <pong.1999@hotmail.com>
2021-03-24 16:05:05 +0700

    Lock function for some condition

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -42,64 +42,113 @@ function Navbarr (props){
         let payload = {
             "token": tk
         }
+        dispatch(signout());
+        dispatch(destroy_token());
+        dispatch(destroy_firstname());
+        dispatch(destroy_lastname());
+        dispatch(destroy_email());
+        dispatch(destroy_age());
+        dispatch(destroy_sex());
+        dispatch(destroy_edu());
+        dispatch(destroy_per());
+        dispatch(destroy_comefrom());
+        dispatch(destroy_school());
+        dispatch(destroy_role());
+        dispatch(signout_admin());
         response = await axios.post("https://spr-system.herokuapp.com/logout/", payload)
         //response = await axios.post("http://127.0.0.1:8000/logout/", payload)
-        if(response.data['status'] === true){
-            dispatch(signout());
-            dispatch(destroy_token());
-            dispatch(destroy_firstname());
-            dispatch(destroy_lastname());
-            dispatch(destroy_email());
-            dispatch(destroy_age());
-            dispatch(destroy_sex());
-            dispatch(destroy_edu());
-            dispatch(destroy_per());
-            dispatch(destroy_comefrom());
-            dispatch(destroy_school());
-            dispatch(destroy_role());
-            dispatch(signout_admin());
-            alert(response.data['message']);
-            //props.history.push('/');
-        }
+        // if(response.data['status'] === true){
+        //     dispatch(signout());
+        //     dispatch(destroy_token());
+        //     dispatch(destroy_firstname());
+        //     dispatch(destroy_lastname());
+        //     dispatch(destroy_email());
+        //     dispatch(destroy_age());
+        //     dispatch(destroy_sex());
+        //     dispatch(destroy_edu());
+        //     dispatch(destroy_per());
+        //     dispatch(destroy_comefrom());
+        //     dispatch(destroy_school());
+        //     dispatch(destroy_role());
+        //     dispatch(signout_admin());
+        //     alert(response.data['message']);
+        //     //props.history.push('/');
+        // }
     }
         return(
             <>
             <Navbar collapseOnSelect expand="lg"  >
-            <Navbar.Brand href="/"  >
-                <i 
-                 className = "fas fa-graduation-cap" Style={{ fontSize:'4.95vw'}}
-                ></i>
-                    <p 
-                    className= "navbar-p" Style={{ fontSize:'1vw'}}
-                    >Study Program</p><p 
-                    className="navbar-p" Style={{ fontSize:'1vw'}}
-                    >Recommendation</p>
-                    <p 
-                    className="navbar-p" Style={{ fontSize:'1vw'}}
-                    >System
-                </p>
+            <Navbar.Brand href="/" >
+                <Nav.Link className="navbar-brand" href="/">
+                    <i className = "fas fa-graduation-cap fa-3x" Style={{ fontSize:'4.95vw'}}>
+                    <span style={{color: 'red', fontSize:'40%'}}>Website Name</span>
+                    </i>
+                </Nav.Link>
+                {/* <div className="row">
+                    <col-2>
+                        <i className = "fas fa-graduation-cap" Style={{ fontSize:'4.95vw'}}/>
+                    </col-2>
+                    <col-10>
+                        <row>
+                            <p className= "navbar-p" Style={{ fontSize:'1vw'}}>Study Program</p>
+                        </row>
+                        <row>
+                            <p className="navbar-p" Style={{ fontSize:'1vw'}}>Recommendation</p>
+                        </row>
+                        <row>
+                            <p className="navbar-p" Style={{ fontSize:'1vw'}}>System</p>
+                        </row>
+                    </col-10>
+                </div> */}
+                
+                    
+                
                 </Navbar.Brand>
 
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav" >
-           
-                <Nav className="mr-auto nav-menu ">
+            {isAdmin ? <Navbar.Collapse id="responsive-navbar-nav" >
+                        <Nav className="mr-auto nav-menu d-flex justify-content-around">
+                    <Nav.Link as={NavLink} to='/Admin_prog' exact  href="/Admin_prog" className= "nav-links" activeClassName="underline" style={{ fontSize:'1vw'}}>Add Course </Nav.Link>
+                    <Nav.Link as={NavLink} to='/Feedbacks' exact  href="/Feedbacks" className= "nav-links" activeClassName="underline" Style={{ fontSize:'1vw'}}>Feedbacks </Nav.Link>
+                    {isLogged ? <NavDropdown style={{ fontSize:'1vw', float: 'right'}}
+                        title="User"
+                        id="collasible-nav-dropdown" 
+                        className="NavDropdown" >
+                            {/* <NavDropdown.Item as={NavLink} to='/AccInfo' className="NavDropdownItem" Style={{ fontSize:'0.9vw'}} >Account Information</NavDropdown.Item> */}
+                            {/* <NavDropdown.Item as={NavLink} to='/Dashboard' className="NavDropdownItem"  Style={{ fontSize:'0.9vw'}} >User Dashboard</NavDropdown.Item> */}
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={NavLink} to='/' className="NavDropdownItem"  tyle={{ fontSize:'0.9vw'}} Style={{ fontSize:'1vw'}} onClick={() => logout(token)} >Log out</NavDropdown.Item>
+                        </NavDropdown> :
+                        <NavDropdown style={{ fontSize:'1vw', align: 'right'}}
+                        title="User"
+                        id="collasible-nav-dropdown" 
+                        className="NavDropdown nav navbar-nav ml-auto w-100 justify-content-end" >
+                            {/* <NavDropdown.Item as={NavLink} to='/AccInfo' className="NavDropdownItem" style={{ fontSize:'0.9vw'}} >Account Information</NavDropdown.Item>
+                            <NavDropdown.Item as={NavLink} to='/Dashboard' className="NavDropdownItem"  style={{ fontSize:'0.9vw'}} style={{ fontSize:'1vw'}}>User Dashboard</NavDropdown.Item>
+                            <NavDropdown.Divider /> */}
+                            <NavDropdown.Item as={NavLink} to='/SignIn' className="NavDropdownItem"  tyle={{ fontSize:'0.9vw'}} Style={{ fontSize:'1vw'}}>Log in</NavDropdown.Item>
+                        </NavDropdown>}
+                </Nav>
+                </Navbar.Collapse>:
+                <Navbar.Collapse id="responsive-navbar-nav" >
+                <Nav className="mr-auto nav-menu d-flex justify-content-around">
                 <Nav.Link as={NavLink} to='/' exact href="/" className= "nav-links" activeClassName="underline" style={{ fontSize:'1vw'}}>About</Nav.Link>
                 <Nav.Link as={NavLink} to='/RecProgram' exact href="/RecProgram" className= "nav-links" activeClassName="underline" Style={{ fontSize:'1vw'}}>Recommend Programs</Nav.Link>
                 <Nav.Link  as={NavLink} to='/Programs' exact href="/Programs" className= "nav-links" activeClassName="underline" Style={{ fontSize:'1vw'}}>Browse Programs</Nav.Link>
-                <Nav.Link as={NavLink} to='/Feedbacks' exact  href="/Feedbacks" className= "nav-links" activeClassName="underline" Style={{ fontSize:'1vw'}}>Feedbacks </Nav.Link>
-                {isAdmin ? <Nav.Link as={NavLink} to='/Admin_prog' exact  href="/Admin_prog" className= "nav-links" activeClassName="underline" style={{ fontSize:'1vw'}}>Add Course </Nav.Link>
-                : null}
+                {/* <Nav.Link className= "nav-links" activeClassName="underline" Style={{ fontSize:'1vw'}}></Nav.Link> */}
+                {/* <Nav.Link className= "nav-links" activeClassName="underline" Style={{ fontSize:'1vw'}}></Nav.Link> */}
+
+                
                 {isLogged ? <NavDropdown style={{ fontSize:'1vw'}}
                 title="User"
                  id="collasible-nav-dropdown" 
                 className="NavDropdown" >
-                    <NavDropdown.Item as={NavLink} to='/AccInfo' className="NavDropdownItem" Style={{ fontSize:'0.9vw'}} >Account Information</NavDropdown.Item>
-                    <NavDropdown.Item as={NavLink} to='/Dashboard' className="NavDropdownItem"  Style={{ fontSize:'0.9vw'}} >User Dashboard</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/AccInfo' className="NavDropdownItem" Style={{ fontSize:'10%'}} >Account Information</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/Dashboard' className="NavDropdownItem"  Style={{ fontSize:'50%'}} >User Dashboard</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item as={NavLink} to='/' className="NavDropdownItem"  tyle={{ fontSize:'0.9vw'}} Style={{ fontSize:'1vw'}} onClick={() => logout(token)} >Log out</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/' className="NavDropdownItem"  tyle={{ fontSize:'0.9vw'}} Style={{ fontSize:'50%'}} onClick={() => logout(token)} >Log out</NavDropdown.Item>
                 </NavDropdown> :
-                <NavDropdown style={{ fontSize:'1vw'}}
+                <NavDropdown style={{ fontSize:'1vw', float: 'right'}}
                 title="User"
                  id="collasible-nav-dropdown" 
                 className="NavDropdown" >
@@ -112,7 +161,7 @@ function Navbarr (props){
                 
                 </Nav>
              
-            </Navbar.Collapse>
+            </Navbar.Collapse>}
             </Navbar>
 
 
