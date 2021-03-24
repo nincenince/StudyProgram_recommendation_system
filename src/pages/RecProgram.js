3e592bcf6175ea891bef54dd9f29acaf595754b9
pong10 <pong.1999@hotmail.com>
2021-03-24 16:05:46 +0700

    Lock function for user and admin

--- src/pages/RecProgram.js
+++ src/pages/RecProgram.js
@@ -7,18 +7,34 @@ import { Col,Row, Button, Image} from "react-bootstrap";
 
 import { useDispatch} from 'react-redux';
 import { update_comefrom } from '../actions';
+// import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 
 function RecProgram (props){
   let dispatch = useDispatch();
-
+  let isLogged = useSelector(state => state.isLogged);
   const changeComefrom = () => {
     dispatch(update_comefrom('recommend'));
   }
   return(
       <>
-      <h1 style={{ fontSize:'2vw', marginLeft:"5%", marginTop:"3%"}}>Get ready to discover your programs</h1>
-        <Row>
+       {isLogged ? <h1 style={{ fontSize:'2vw', marginLeft:"5%", marginTop:"3%"}}>Get ready to discover your programs</h1>
+       :<div> 
+          <Row style={{justifyContent:'center', alignItems:'center'}}>
+            <h1 style={{ fontSize:'2vw', marginTop:"3%"}}>Please Signup or Login to use this service.</h1>
+            <h2 style={{ fontSize:'2vw', marginTop:"3%"}}>This button will redirect you to Signup/Login page</h2>
+          </Row>
+          <Row style={{justifyContent:'center', alignItems:'center'}}>
+            <h3 style={{ fontSize:'2vw', marginTop:"3%"}}>----==============----</h3>
+          </Row>
+          <Row style={{justifyContent:'center', alignItems:'center'}}>
+              <a href='/SignIn'>
+                <button style={{backgroundColor: 'red', borderColor: 'red', color: 'white',padding:'5% 2% 5% 2%', width: '20vw',borderRadius: '5px', fontSize:'2vw', marginTop:"3%"}}>Signup/Login</button>
+              </a>
+          </Row>
+        </div>}
+       {isLogged ?<Row>
           <Col md={{ span: 3, offset: 3 }}>
             <div style={{border:'solid', borderWidth:'thin',marginTop:'15%', padding:'10%'}}>
               <Image style={{widht:'3vw', height:'7vh', paddingLeft:'35%'}} src={require("../images/1.png")}/>
@@ -45,9 +61,9 @@ function RecProgram (props){
             </div>
           </Col>
 
-        </Row>
+        </Row>: null }
 
-        <Row>
+        {isLogged ?<Row>
           <Col  md={{ span: 3, offset: 5}}>
             <Link to="/RecResult">
               <Button style={{ fontSize:'1vw', marginTop:'10%',borderRadius:' 20px', backgroundColor:'rgb(255, 70, 0)',border:'coral'}}>
@@ -55,7 +71,7 @@ function RecProgram (props){
               </Button>
             </Link>
           </Col>
-        </Row>
+        </Row>: null }
       </>
   )
 }
