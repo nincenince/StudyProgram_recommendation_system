b3c7b84bdb7cd0f39988de8523fa0e448dfa21bf
pong10 <pong.1999@hotmail.com>
2021-04-19 03:22:22 +0700

    Add change profile picture support

--- src/pages/AccInfo.js
+++ src/pages/AccInfo.js
@@ -4,12 +4,12 @@ import './AccInfo.css';
 import { Col,Row, Container , Form, Button, Navbar} from "react-bootstrap";
 
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch} from 'react-redux';
-import { update_firstname, update_lastname, update_email, update_sex, update_school } from '../actions';
+import { update_firstname, update_lastname, update_email, update_sex, update_school, update_profilepic } from '../actions';
 
 
-function AccInfo (props) {
+function AccInfo (props,defaultImage) {
   //   constructor() {
   //   super();
   //   this.state = {
@@ -21,10 +21,21 @@ function AccInfo (props) {
   //   this.handleSubmit = this.handleSubmit.bind(this);
   // }
 
+
+
   let token = useSelector(state => state.token);
   let isLogged = useSelector(state => state.isLogged);
   let dispatch = useDispatch();
   let personal = useSelector(state => state.personal);
+
+  const cloudinary = require('cloudinary/lib/cloudinary').v2;
+  const fileSelect = useRef(null);
+  const [image, setImage] = useState("https://res.cloudinary.com/hdtjuro73/image/upload/v1617866916/"+personal.profilepic);
+  const [initpic, setinitpic] = useState("https://res.cloudinary.com/hdtjuro73/image/upload/v1617866916/"+personal.profilepic);
+  const [progress, setProgress] = useState(0);
+  const [Publicid, setPublicid]=  useState('');
+  const [profilepic, setprofilepic] = useState(personal.profilepic);
+
   let errors = {};
   useEffect(() => {
     //token = useSelector(state => state.token);
@@ -54,7 +65,7 @@ function AccInfo (props) {
   // const handleChange = (event) => {
   //   setnewemail("");
   // }
-     
+
   const handlePersonalSubmit = (event) => {
     event.preventDefault();
     if(firstname === ''){
@@ -69,15 +80,15 @@ function AccInfo (props) {
     if(gender === null || gender === ''){
       setgender(personal.sex);
     }
-    validate(token,false,null,false,null,null,null,true,firstname,true,lastname,true,school,true,gender);
+    validate(token,false,null,false,null,null,null,true,firstname,true,lastname,true,school,true,gender,false,null);
   }
   const handleEmailSubmit = (event) => {
     event.preventDefault();
-    validate(token,true,newemail,false,null,null,null,false,null,false,null,false,null,false,null);
+    validate(token,true,newemail,false,null,null,null,false,null,false,null,false,null,false,null,false,null);
   }
   const handlePasswordSubmit = (event) => {
     event.preventDefault();
-    validate(token,false,null,true,newpassword,confirmnewpassword,oldpassword,false,null,false,null,false,null,false,null);
+    validate(token,false,null,true,newpassword,confirmnewpassword,oldpassword,false,null,false,null,false,null,false,null,false,null);
   }
   
   // const handleSubmit = (event) => {
@@ -95,8 +106,108 @@ function AccInfo (props) {
   //       alert('Demo Form is submited');
   //   }
   // }
+  async function handleImageUpload() {
+    if (fileSelect) {
+      fileSelect.current.click();
+    }
+  }
+
+  function handleFiles(files) {
+    for (let i = 0; i < files.length; i++) {
+      console.log(files[i]);
+      uploadFile(files[i]);
+    }
+  }
+  const handleProfilePicSubmit = () => {
+    setcodes(4);
+
+    setPublicid(personal.profilepic);
+    console.log("personal profile pic: "+personal.profilepic);
+    console.log("From submit: "+Publicid)
+    deletepic(personal.profilepic);
+    
+    validate(token,false,null,false,false,null,null,false,null,false,null,false,null,false,null,true,profilepic);
+    
+  }
   
-  const validate = async (tk,ec,ne,pc,np,cnp,op,fc,nf,lc,nl,sc,ns,gc,ng) => {
+  function uploadFile(file) {
+    const url = `https://api.cloudinary.com/v1_1/hdtjuro73/image/upload/`;
+    // https://res.cloudinary.com/demo/image/upload/w_70,h_53,c_scale/turtles.jpg
+    const xhr = new XMLHttpRequest();
+    const fd = new FormData();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+    // Update progress (can be used to show progress indicator)
+    xhr.upload.addEventListener("progress", (e) => {
+      setProgress(Math.round((e.loaded * 100.0) / e.total));
+      console.log(Math.round((e.loaded * 100.0) / e.total));
+    });
+
+    xhr.onreadystatechange = (e) => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        setPublicid(response.public_id);
+        let a = response.secure_url.split('/');
+        setprofilepic(a[7]);
+        setImage(response.secure_url);
+        
+        console.log("From upload: ");
+        console.log(Publicid);
+        console.log(response);
+      }
+    };
+
+    fd.append(
+      "upload_preset",
+      'rkoa3ql1'
+    );
+    fd.append(
+      "options",
+      {width: 150, height: 150, gravity: "face", crop: "thumb"}
+    )
+    // {width: 150, height: 150, gravity: "face", crop: "thumb"}
+    // cloudinary.image(file, {secure: true, transformation: [
+    //   {width: 150, height: 150, gravity: "face", crop: "thumb"},
+    //   {radius: 20},
+    //   {effect: "sepia"},
+    //   {overlay: "cloudinary_icon_blue", gravity: "south_east", x: 5, y: 5, width: 50, opacity: 60, effect: "brightness:200"},
+    //   {angle: 10}
+    //   ]})
+    // fd.append("tags", "browser_upload");
+    fd.append('tags', 'User_profile_pics');
+    fd.append("file", file);
+    xhr.send(fd);
+  }
+  const deletepic = async (a) => {
+    cloudinary.config({ 
+      cloud_name: 'hdtjuro73', 
+      api_key: '946796654646798', 
+      api_secret: '-vPJzL6YrTpc4Ef6KO9XNqKXJ5I' 
+    });
+    
+    console.log("From delete: "+a);
+
+    await cloudinary.uploader.destroy(a,
+      function(err, res) {
+        console.log(res);
+      }
+    );
+    setPublicid('');
+    setProgress(0);
+  }
+  const cancelchange = () => {
+    if(Publicid === ''){
+      setImage(initpic);
+    }
+    else{
+      let a = Publicid;
+      deletepic(a);
+      setImage(initpic);
+    }
+    
+  }
+  const validate = async (tk,ec,ne,pc,np,cnp,op,fc,nf,lc,nl,sc,ns,gc,ng,pfpc,npfp) => {
       let input = {};
       input['email'] = ne;
       input['oldpassword'] = op;
@@ -169,6 +280,7 @@ function AccInfo (props) {
           }
         } 
       }
+ 
       // alert(isValid);
       if(isValid) {
         let payload = {
@@ -185,11 +297,13 @@ function AccInfo (props) {
           "school_change": sc,
           "new_school": ns,
           "gender_change": gc,
-          "new_gender": ng
+          "new_gender": ng,
+          "profilepic_change": pfpc,
+          "new_profilepic": npfp
         }
         console.log(payload);
-        response = await axios.post("https://spr-system.herokuapp.com/edit/personal/info/", payload)
-        //response = await axios.post("http://127.0.0.1:8000/edit/personal/info/", payload)
+        //response = await axios.post("https://spr-system.herokuapp.com/edit/personal/info/", payload)
+        response = await axios.post("http://127.0.0.1:8000/edit/personal/info/", payload)
       }
       else {
         return isValid;
@@ -202,6 +316,7 @@ function AccInfo (props) {
         dispatch(update_email(response.data['info']['email']));
         dispatch(update_sex(response.data['info']['sex']));
         dispatch(update_school(response.data['info']['school']));
+        dispatch(update_profilepic(response.data['info']['profilepic']))
         alert(response.data['message']);
         response = {};
         props.history.push("/AccInfo");
@@ -225,22 +340,71 @@ function AccInfo (props) {
      
     return (
       <div>
-          <Navbar collapseOnSelect expand="lg"  >
+          {/* <Navbar collapseOnSelect expand="lg"  >
             <Navbar.Brand href="/"  >
                 <i 
                  className = "fas fa-graduation-cap" style={{ fontSize:'4.95vw'}}
                 ></i>
                    <h5 style={{color:'coral'}}>website name</h5>
                 </Navbar.Brand>
-            </Navbar>
+            </Navbar> */}
 
           <Container>
             <Col sm={3}></Col>
             <Col sm={5}>
+              
               {/* <Row>  */}
                   <h2 style={{marginTop:'10%'}}>User Information</h2>
               {/* </Row> */}
-            
+              <Form onSubmit={handlePersonalSubmit}>
+                <Form.Group controlId="forpfp">
+                  <Form.Label >Profile image</Form.Label>
+                </Form.Group>
+              </Form>
+
+              <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+                <img className="object-contain rounded-lg" src={image.replace("upload/", "upload/w_250,c_fill,ar_1:1,g_auto,r_max,q_auto,f_auto/")}/>
+              </div>
+              <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+                <span className="text-gray-700">Upload status: {progress}%</span>
+              </div>
+              
+              <div className="bg-gray-200 border-4 border-dashed border-gray-400 rounded-lg" >
+                <form className="flex justify-center items-center h-full">
+                  <div className="text-center justify-center items-center">
+                      <Button
+                        style={{display:"flex", alignItems:"center", justifyContent:"center"}}
+                        variant="warning"
+                        className="bg-blue-600 hover:bg-blue-800 text-white font-bold rounded block m-auto"
+                        onClick={handleImageUpload}
+                        type="button"
+                      >
+                        Browse
+                      </Button>
+                      <hr></hr>
+                      
+                      <Button  className="acc-done-button" onClick={() => handleProfilePicSubmit()}>
+                          Save change
+                      </Button>
+                      <span>&nbsp;</span>
+                      <Button  className="acc-done-button" style={{backgroundColor: 'grey', border: 'black'}} onClick={() => cancelchange()}>
+                          Cancel
+                      </Button>
+                      <br></br>
+                    </div>
+                    <br></br><br></br>
+                
+                  
+                  <input
+                    ref={fileSelect}
+                    type="file"
+                    accept="image/*"
+                    style={{ display: "none" }}
+                    onChange={(e) => handleFiles(e.target.files)}
+                  />
+                </form>
+              </div>
+
               <Form onSubmit={handlePersonalSubmit}>
                 
                 <Form.Group controlId="forfn">
@@ -263,7 +427,7 @@ function AccInfo (props) {
 
               <Form.Label >Gender</Form.Label>
                 <Form.Group  controlId="forgd">
-                  <Form.Control as="select" defaultValue={personal.sex}  placeholder={personal.sex} value={gender} onChange={e => setgender(e.target.value)} >
+                  <Form.Control as="select" defaultValue={personal.sex} value={gender} onChange={e => setgender(e.target.value)} >
                     <option >{null}</option>
                     <option >Female</option>
                     <option >Male</option>
@@ -276,7 +440,7 @@ function AccInfo (props) {
 
                 <Form.Label >School</Form.Label>
                 <Form.Group  controlId="forsc">
-                  <Form.Control as="select" defaultValue={personal.school} placeholder={personal.school} value={school} onChange={e => setschool(e.target.value)}>
+                  <Form.Control as="select" defaultValue={personal.school} value={school} onChange={e => setschool(e.target.value)}>
                     <option >{null}</option>
                     <option >School1</option>
                     <option >School2</option>
@@ -290,7 +454,7 @@ function AccInfo (props) {
                 {/* <Link> */}
                 {/* <Link to="/RecProgram"> */}
                 <Button type="Submit" className="acc-done-button" onClick={() => setcodes(1)}>
-                    Done
+                    Save change
                 </Button>
                 {/* </Link> */}
 
@@ -332,7 +496,7 @@ function AccInfo (props) {
                 </Form.Group>
 
                 <Button style={{display:"flex", alignItems:"center", justifyContent:"center"}} type="submit"className="update-email-button" onClick={() => setcodes(2)}>
-                  Update Email
+                  Save change
                 </Button>
                 <br/>
               </Form>
@@ -357,7 +521,7 @@ function AccInfo (props) {
                   </Form.Text>
                 </Form.Group>
                 <Button style={{display:"flex", alignItems:"center", justifyContent:"center"}} className="update-pas-button"type="submit" onClick={() => setcodes(3)}>
-                  Update Password
+                  Save change
                 </Button>
               </Form>
 
